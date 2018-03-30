d0536e389c1883777c7fc001f9b84b5953c107e6
spursy <582214816@qq.com>
2018-03-30 11:13:57 +0800

    add views

--- config/config.default.js
+++ config/config.default.js
@@ -1,2 +1,9 @@
 // config/config.default.js
 exports.keys = "spursyy"; //此处改为你自己的 Cookie 安全字符串
+
+exports.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+        '.tpl': 'nunjucks',
+    },
+};
