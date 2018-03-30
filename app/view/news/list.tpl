67132feda2d093dc548f81eab2476b379c1f2640
spursy <582214816@qq.com>
2018-03-30 14:31:48 +0800

    add test file

--- app/view/news/list.tpl
+++ app/view/news/list.tpl
@@ -8,6 +8,7 @@
     <ul class="news-view view">
       {% for item in list %}
         <li class="item">
+        {{ helper.relativeTime(item.time) }}
           <a href="{{ item.url }}">{{ item.title }}</a>
         </li>
       {% endfor %}
--- config/config.default.js
+++ config/config.default.js
@@ -7,3 +7,17 @@ exports.view = {
         '.tpl': 'nunjucks',
     },
 };
+exports.news = {
+    pageSize: 5,
+    serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
+// add middleware robot
+exports.middleware = [
+    'robot'
+];
+// robot's configurations
+exports.robot = {
+    ua: [
+        /Baiduspider/i,
+    ]
+};
